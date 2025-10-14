8640710c665c2633ca32a0d7147690d45de18771
Dale Louize Almonia <dmalmonia@up.edu.ph>
2025-10-14 23:36:09 +0800

    Updated Backend

--- account.py
+++ account.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "13c4f3f7e6f6ff70a262eae263c51b8c"
 
 db = SQLAlchemy(app)
 
